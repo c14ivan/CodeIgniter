373d7bc8de83da8277af8a81728eb8320d1433f6
c14nuro <tics@ieflafloresta.edu.co>
2013-06-17 15:45:29 -0500

    starting perms

--- application/config/permissions.php
+++ application/config/permissions.php
@@ -1,3 +1,17 @@
 <?php
 
 $config['context_levels'] = array(10=>'CONTEXT_SYSTEM',20=>'CONTEXT_USER',30=>'CONTEXT_MODULE',40=>'CONTEXT_SUBMODULE');
+
+$config['default-roles']=array(
+        array('name'=>'Superuser','weight'=>50,'shortname'=>'super','description'=>''),
+        array('name'=>'Administrator','weight'=>40,'shortname'=>'admin','description'=>''),
+        array('name'=>'Editing Teacher','weight'=>30,'shortname'=>'teacheredt','description'=>''),
+        array('name'=>'Teacher','weight'=>20,'shortname'=>'teacher','description'=>''),
+        array('name'=>'Student','weight'=>10,'shortname'=>'student','description'=>'')
+        );
+
+$config['default-capabilities']=array(
+        array('capability'=>'users/list','url'=>'auth/users','weight'=>30),
+        array('capability'=>'users/add','url'=>'auth/users','weight'=>30),
+        array('capability'=>'users/enrol','url'=>'','weight'=>30),
+        );
--- application/sql/permissions.sql
+++ application/sql/permissions.sql
@@ -3,6 +3,7 @@ delimiter $$
 CREATE TABLE `role` (
   `id` bigint(10) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `weight` bigint(10) NOT NULL DEFAULT '0',
   `shortname` varchar(100) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
   `description` longtext CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id`),
@@ -22,7 +23,17 @@ CREATE TABLE `role_assignments` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_bin$$
 
+delimiter $$
 
+CREATE TABLE `capabilities` (
+  `id` bigint(10) NOT NULL AUTO_INCREMENT,
+  `capability` bigint(10) NOT NULL DEFAULT '0',
+  `url` varchar(255) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `weight` bigint(10) NOT NULL DEFAULT '0',
+  `timemodified` bigint(10) NOT NULL DEFAULT '0',
+  `modifierid` bigint(10) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_bin$$
 
 delimiter $$
 
@@ -30,8 +41,7 @@ CREATE TABLE `role_capabilities` (
   `id` bigint(10) NOT NULL AUTO_INCREMENT,
   `contextid` bigint(10) NOT NULL DEFAULT '0',
   `roleid` bigint(10) NOT NULL DEFAULT '0',
-  `capability` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
-  `url` varchar(255) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `capabilityid` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
   `icon` varchar(45) COLLATE utf8_bin DEFAULT NULL,
   `permission` bigint(10) NOT NULL DEFAULT '0',
   `timemodified` bigint(10) NOT NULL DEFAULT '0',
