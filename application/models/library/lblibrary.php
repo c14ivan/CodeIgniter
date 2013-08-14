f49d0113322461a1ce088313a6b925d3d1852439
Ivan <tics@ieflafloresta.edu.co>
2013-08-14 08:05:49 -0500

    library bug deleted

--- application/models/library/lblibrary.php
+++ application/models/library/lblibrary.php
@@ -9,21 +9,30 @@ class Lblibrary extends CI_Model{
         $query=$this->db->get($this->tablecats);
         $firstsumary=array();
         $response=array();
-        foreach ($query->result_array() as $cat){
-            $response[$cat['id']]=array('id'=>$cat['id'],'name'=>$cat['name'],'ident'=>$cat['catid'],'childrens'=>array());
-            $firstsumary[$cat['id']]=$cat['id'];
+        if($query->num_rows()>0){
+            foreach ($query->result_array() as $cat){
+                $response[$cat['id']]=array('id'=>$cat['id'],'name'=>$cat['name'],'ident'=>$cat['catid'],'childrens'=>array());
+                $firstsumary[$cat['id']]=$cat['id'];
+            }
         }
-        $this->db->where_in('catparent',$firstsumary);
-        $query=$this->db->get($this->tablecats);
-        foreach ($query->result_array() as $cat){
-            $response[$cat['catparent']]['childrens'][$cat['id']]=array('id'=>$cat['id'],'name'=>$cat['name'],'ident'=>$cat['catid'],'childrens'=>array());
-            $secondsumary[$cat['id']]=$cat['catparent'];
+        if(count($firstsumary)>0){
+            $this->db->where_in('catparent',$firstsumary);
+            $query=$this->db->get($this->tablecats);
+            if($query->num_rows()>0){
+                foreach ($query->result_array() as $cat){
+                    $response[$cat['catparent']]['childrens'][$cat['id']]=array('id'=>$cat['id'],'name'=>$cat['name'],'ident'=>$cat['catid'],'childrens'=>array());
+                    $secondsumary[$cat['id']]=$cat['catparent'];
+                }
+            }
         }
-        
-        $this->db->where_in('catparent',array_keys($secondsumary));
-        $query=$this->db->get($this->tablecats);
-        foreach ($query->result_array() as $cat){
-            $response[$secondsumary[$cat['catparent']]]['childrens'][$cat['catparent']]['childrens'][$cat['id']]=array('id'=>$cat['id'],'name'=>$cat['name'],'ident'=>$cat['catid'],'childrens'=>array());
+        if(count($secondsumary)>0){
+            $this->db->where_in('catparent',array_keys($secondsumary));
+            $query=$this->db->get($this->tablecats);
+            if($query->num_rows()>0){
+                foreach ($query->result_array() as $cat){
+                    $response[$secondsumary[$cat['catparent']]]['childrens'][$cat['catparent']]['childrens'][$cat['id']]=array('id'=>$cat['id'],'name'=>$cat['name'],'ident'=>$cat['catid'],'childrens'=>array());
+                }
+            }
         }
         return $response;
     }
